fb6b40d90b0a632bfe25683ea707a7e8fe694fa1
jellymando <cicikdi@naver.com>
2021-11-26 22:11:18 +0900

    fix: search api 수정

--- api/search.js
+++ api/search.js
@@ -4,10 +4,10 @@ import convert from 'xml-js';
 const search = async (keyword) => {
     try {
         const result = await axios.get(
-            `/api?key=${process.env.NEXT_PUBLIC_API_KEY}&apiCode=ProductSearch&keyword=${
-                keyword ? keyword : '여성의류'
-            }&option=Categories`,
-            { headers: { 'content-type': 'application/json' } }
+            `https://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=${
+                process.env.NEXT_PUBLIC_API_KEY
+            }&apiCode=ProductSearch&keyword=${keyword ? keyword : encodeURI('여성의류')}&option=Categories`,
+            { headers: { 'content-type': 'application/json;charset=UTF-8' } }
         );
         return JSON.parse(convert.xml2json(result.data, { compact: true, spaces: 4 })).ProductSearchResponse.Products
             .Product;
