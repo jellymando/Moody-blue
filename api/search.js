c82a15c6561a7212b15648796dc8d1dec1467a9c
jellymando <cicikdi@naver.com>
2021-12-03 14:23:27 +0900

    feat: 환경 변수명 수정

--- api/search.js
+++ api/search.js
@@ -5,7 +5,7 @@ const search = async (keyword) => {
     try {
         const result = await axios.get(
             `https://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=${
-                process.env.NEXT_PUBLIC_API_KEY
+                process.env.NEXT_PUBLIC_11ST_API_KEY
             }&apiCode=ProductSearch&keyword=${keyword ? keyword : encodeURI('여성의류')}&option=Categories`,
             { headers: { 'content-type': 'application/json;charset=UTF-8' } }
         );
