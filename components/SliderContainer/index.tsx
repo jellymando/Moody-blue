3f5a400ed62e7e24ee3db45076fd8e42b90f610d
jellymando <cicikdi@naver.com>
2022-01-15 21:20:54 +0900

    feat: SliderContainer 추가

--- components/SliderContainer/index.tsx
+++ components/SliderContainer/index.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import Slider from 'react-slick';
+import { Slide, Image } from './styled';
 
 export default function SliderContainer() {
     const settings = {
-        dots: true,
         infinite: true,
         autoplay: true,
-        speed: 2000,
-        autoplaySpeed: 2000,
-        cssEase: 'linear',
+        speed: 500,
+        autoplaySpeed: 5000,
+        pauseOnHover: false,
     };
     return (
         <Slider {...settings}>
-            <div>
-                <h3>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
-            <div>
-                <h3>4</h3>
-            </div>
-            <div>
-                <h3>5</h3>
-            </div>
-            <div>
-                <h3>6</h3>
-            </div>
+            <Slide>
+                <Image src="/images/promotion/promotion_01.jpg" />
+            </Slide>
+            <Slide>
+                <Image src="/images/promotion/promotion_02.jpg" />
+            </Slide>
+            <Slide>
+                <Image src="/images/promotion/promotion_03.jpg" />
+            </Slide>
+            <Slide>
+                <Image src="/images/promotion/promotion_04.jpg" />
+            </Slide>
         </Slider>
     );
 }
---
+++ components/SliderContainer/styled.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import 'slick-carousel/slick/slick.css';
+
+export const Slide = styled.div``;
+
+export const Image = styled.img`
+    width: 100%;
+    height: auto;
+`;
