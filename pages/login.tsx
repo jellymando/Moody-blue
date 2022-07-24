da33f6edf84dd55ae4ce7fe2be0aa019d87f24c3
jellymando <cicikdi@naver.com>
2022-07-24 18:17:26 +0900

    feat: 로그인 페이지 디자인시스템 업데이트 적용

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import Link from 'next/link';
 import styled from '@emotion/styled';
-import { Button, Input, List } from 'jelly-design-system';
+import { Button, Input } from 'jelly-design-system';
 import { Wrapper, MainTitle } from 'styles/contents';
 import Layout from 'components/Layout';
 
@@ -17,22 +18,42 @@ const LoginForm = styled.form`
     width: 100%;
 `;
 
+const Ul = styled.ul``;
+
+const Li = styled.li`
+    margin-bottom: 20px;
+
+    &.login-button {
+        margin-bottom: 15px;
+    }
+`;
+
 const Login = () => {
     return (
         <Layout>
             <LoginWrapper>
                 <LoginForm>
                     <MainTitle>로그인</MainTitle>
-                    <List>
-                        <Input label="아이디" />
-                        <Input type="password" label="비밀번호" />
-                        <Button type="submit" color="mellowBlue" filled>
-                            로그인
-                        </Button>
-                        <Button type="button" color="mellowBlue">
-                            회원가입
-                        </Button>
-                    </List>
+                    <Ul>
+                        <Li>
+                            <Input label="아이디" inline />
+                        </Li>
+                        <Li>
+                            <Input type="password" label="비밀번호" inline />
+                        </Li>
+                        <Li className="login-button">
+                            <Button type="submit" color="mellowBlue" filled>
+                                로그인
+                            </Button>
+                        </Li>
+                        <Li>
+                            <Link href="/join">
+                                <Button type="button" color="mellowBlue">
+                                    회원가입
+                                </Button>
+                            </Link>
+                        </Li>
+                    </Ul>
                 </LoginForm>
             </LoginWrapper>
         </Layout>
